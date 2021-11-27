65bffba583826c9a702e6486c3599ee5224d75d5
xiaowu <447926704@qq.com>
2021-11-27 21:20:05 +0800

    解决只能将测试结果写入一个表中的问题

--- common/operate_excel.py
+++ common/operate_excel.py
@@ -14,22 +14,23 @@ excel_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file
 
 
 class OperateExcel:
-    def __init__(self, sheet_name):
+    def __init__(self, sheet_name, file_name=excel_path):
         """
         初始化数据
         """
-        self.path = excel_path
-        self.wb = openpyxl.load_workbook(self.path)
-        self.wk = self.wb[sheet_name]
+        self.file = file_name
+        self.sheet_name = sheet_name
 
     def read_excel(self):
         """
         将所有用例按字典格式转化后加入到列表data_all
         :return: data_all
         """
-        # 通过遍历行读取Excel数据并将每行加入到一个列表row_list，wk.rows相当于一个生成器
+        wb = openpyxl.load_workbook(self.file)
+        ws = wb[self.sheet_name]
+        # 通过遍历行读取Excel数据并将每行加入到一个列表row_list，ws.rows相当于一个生成器
         data_list = []
-        for row in self.wk.rows:
+        for row in ws.rows:
             row_list = []
             for cell in row:
                 row_list.append(cell.value)
@@ -39,7 +40,7 @@ class OperateExcel:
         for i in data_list[1:]:
             x = dict(zip(data_list[0], i))
             data_all.append(x)
-        self.wb.close()
+        wb.close()
         return data_all
 
     def write_excel(self, row, actually, result):
@@ -50,12 +51,14 @@ class OperateExcel:
         :param actually:要写入的数据
         :return:
         """
-        # print(row, actually, result)
-        self.wk["H{}".format(row)] = actually
-        self.wk["I{}".format(row)] = result
-        self.wb.save(self.path)
-        self.wb.close()
-        print("测试结果写完关闭")
+        wb = openpyxl.load_workbook(self.file)
+        ws = wb[self.sheet_name]
+        # self.ws.cell(row=row, column=8).value = actually
+        # self.ws.cell(row=row, column=9).value = result
+        ws["H{}".format(row)] = actually
+        ws["I{}".format(row)] = result
+        wb.save(self.file)
+        wb.close()
 
     def write_header(self, header):
         """
@@ -63,9 +66,9 @@ class OperateExcel:
         :param header: header值
         :return:
         """
-        for i in range(2, self.wk.max_row + 1):
-            self.wk["E{}".format(i)] = header
-        self.wb.save(self.path)
-        self.wb.close()
-        print("header写完关闭")
-
+        wb = openpyxl.load_workbook(self.file)
+        ws = wb[self.sheet_name]
+        for i in range(2, ws.max_row + 1):
+            ws["E{}".format(i)] = header
+        wb.save(self.file)
+        wb.close()
